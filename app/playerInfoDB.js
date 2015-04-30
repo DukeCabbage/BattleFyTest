a00b78cdf3e28c5f0a11ce2bb3e97e933087a13f
DukeCabbage <liaowng@gmail.com>
2015-04-29 18:29:51 -0700

    db

---
+++ app/playerInfoDB.js
@@ -0,0 +1,18 @@
+var db = require('monk')('localhost:27017/nodetest1');
+var collection = db.get('usercollection');
+
+exports.find = function(key, value, callback, res) {
+	var obj = {};
+	obj[key] = value;
+	
+	collection.find(obj, function(err, items){
+		callback(items, res);
+	});
+	
+};
+
+exports.insert = function(obj, callback) {
+	collection.insert(obj, function(err, result){
+		callback();
+	})
+};
--- server.js
+++ server.js
@@ -4,36 +4,28 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var exphbs = require('express3-handlebars');
 
-var routes = require('./app/routes');
-var query = require('./app/playerQuery')
-
 var app = express();
 
 var port = 8080;
 var domain = 'localhost';
-var _scope = this;
 
-var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost:27017/nodetest1", mongoDBOnConnect);
-
-function mongoDBOnConnect(err, db) {
-  if(!err) {
-    console.log("We are connected");
-    _scope.testDB = db;
-    var collection = db.collection('usercollection');
-    var stream = collection.find().stream();
-    stream.on("data", function(item) {
-      console.log(item);
-      console.log(item['_id']);
-    });
-    stream.on("end", function() {
-      console.log('end');
-    });
-
-  }else{
-    return console.dir(err);
-  }
+var findCallback = function(items, res){
+    if (items.length == 0){
+        res.writeHead(404);
+        res.end('User not found');
+    }else{
+        res.writeHead(404);
+        res.end(items[0]['email']);
+        console.log(items[0]['email']);
+    }
 }
+
+var playerInfoDB = require('./app/playerInfoDB');
+
+app.get('/user/:name', function(req, res){
+    playerInfoDB.find('username', req.params.name, findCallback, res);
+});
+
 
 app.set('port', port);
 app.use(logger('dev'));
@@ -45,14 +37,9 @@ app.set('views', __dirname + '/views');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// Make our db accessible to our router
-// app.use(function(req,res,next){
-//     req.db = db;
-//     next();
-// });
-
-app.use('/', routes);
-app.use('/player', query);
+// routes for serving pages and handling searches
+app.use('/', require('./app/routes'));
+app.use('/player', require('./app/playerQuery'));
 
 app.listen(port, domain);
 console.log('Server up: http://'+domain+':'+app.get('port'));
@@ -64,29 +51,3 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-    // app.use(function(err, req, res, next) {
-    //     res.status(err.status || 500);
-    //     res.render('error', {
-    //         message: err.message,
-    //         error: err
-    //     });
-    // });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
-
-
